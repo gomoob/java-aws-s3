354ff5a58a6a93c391f00479f3b5141ee4aa95ea
JiamingL <jiaming.liang@gomoob.com>
2017-09-05 11:29:56 +0200

    Add S3Client.

---
+++ src/main/java/com/gomoob/aws/s3client/S3Client.java
@@ -0,0 +1,31 @@
+/**
+ * // * Copyright 2017 ATK EVENTS SAS. All rights reserved. //
+ */
+// package com.gomoob.aws.s3client;
+//
+// import org.springframework.stereotype.Component;
+//
+// import com.amazonaws.AmazonServiceException;
+// import com.amazonaws.SdkClientException;
+// import com.amazonaws.services.s3.AmazonS3Client;
+// import com.amazonaws.services.s3.model.PutObjectRequest;
+// import com.amazonaws.services.s3.model.PutObjectResult;
+//
+/// **
+// * An implementation for the <tt>IS3</tt> interface which uses acts as a mock and allow to write unit test on Amazon
+// S3
+// * JAVA SDK functions without being connected to S3.
+// *
+// * @author Jiaming LIANG (jiaming.liang@gomoob.com)
+// */
+// @Component
+// public class S3Client extends AmazonS3Client {
+//
+// @Override
+// public PutObjectResult putObject(PutObjectRequest putObjectRequest)
+// throws SdkClientException, AmazonServiceException {
+// // TODO Auto-generated method stub
+// System.out.println("In the method `putObject` of the S3Client.");
+// return null;
+// }
+// }
